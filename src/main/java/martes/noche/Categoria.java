042eb2a2aca6df9a693b42bd9241e0f5aa7fe9c1
Juan Martin Conde <juanma.conde@hotmail.com>
2018-04-18 20:58:12 -0300

    se agrega funcionalidad clase Categoria

--- src/main/java/martes/noche/Categoria.java
+++ src/main/java/martes/noche/Categoria.java
@@ -1,6 +1,15 @@
 package martes.noche;
 
-public class Categoria {
+public abstract class Categoria {
+    private int cargo_fijo;
+    private int cargo_variable;
 
+    public int getCargo_fijo(){return cargo_fijo;}
+
+    public void setCargo_fijo(int cargo_fijo) { this.cargo_fijo = cargo_fijo; }
+
+    public int getCargo_variable() { return cargo_variable;}
+
+    public void setCargo_variable(int cargo_variable) { this.cargo_variable = cargo_variable; }
 
 }
