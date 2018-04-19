1afff03be7d17a03a717364b6521bfc611ac6840
Juan Martin Conde <juanma.conde@hotmail.com>
2018-04-18 23:21:04 -0300

    Se agrega test y clase de 1 categoria

--- src/main/java/martes/noche/Categoria.java
+++ src/main/java/martes/noche/Categoria.java
@@ -1,8 +1,8 @@
 package martes.noche;
 
 public abstract class Categoria {
-    private int cargo_fijo;
-    private int cargo_variable;
+    public int cargo_fijo;
+    public int cargo_variable;
 
     public int getCargo_fijo(){return cargo_fijo;}
 
@@ -11,5 +11,7 @@ public abstract class Categoria {
     public int getCargo_variable() { return cargo_variable;}
 
     public void setCargo_variable(int cargo_variable) { this.cargo_variable = cargo_variable; }
+
+    public void calcular_valor_aprox (){};
 
 }
