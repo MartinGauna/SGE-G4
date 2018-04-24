06878d6409e97e27402bc4d17b4fcaee460c9aea
Juan Martin Conde <juanma.conde@hotmail.com>
2018-04-23 23:13:07 -0300

    Se agrega lectura de JSON para dispositivo - sera revisada

--- src/main/java/martes/noche/Dispositivo.java
+++ src/main/java/martes/noche/Dispositivo.java
@@ -1,5 +1,11 @@
 package martes.noche;
 
+//TODO: ver porque no importa el gson
+//import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
 public class Dispositivo {
 
     private String nombre;
@@ -35,4 +41,31 @@ public class Dispositivo {
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
+//TODO: descomentar codigo cuando resuelva el primer problema y ver si funciona
+/*    public static void leerJSON() {
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("D:\\staff.json")) {
+
+            // Convert JSON to Java Object
+            Dispositivo dispositivo = gson.fromJson(reader, dispositivo.class);
+            System.out.println(dispositivo);
+
+            // Ejemplo de JSON a importar
+            *//*
+              "dispositivo": [
+                 {
+                  "nombre": "Play Station 5",
+                  "consumoHora": 5000,
+                  "estado": "activo",
+                 }]
+            *//*
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }*/
+
 }
