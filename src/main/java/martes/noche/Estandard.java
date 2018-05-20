88c3ea7b777187048812b6718b240330560143ee
Juan Martin Conde <juanma.conde@hotmail.com>
2018-05-20 20:52:50 -0300

    se agrega set en estandar

--- src/main/java/martes/noche/Estandard.java
+++ src/main/java/martes/noche/Estandard.java
@@ -5,5 +5,10 @@ public class Estandard extends Dispositivo {
     //Class Properties
     //Note: Dispositivo properties (inherating): String nombre, int consumoHora,  String estado;
 
+    public int consumoEstimado;
+
+    public void setConsumoEstimado(int consumoEstimado) {
+        this.consumoEstimado = consumoEstimado;
+    }
 
 }
