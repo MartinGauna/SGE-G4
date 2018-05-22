13fa163a936e58b274f825c41d46a98980b1e5ad
Gonzalo Giliberti <gonzalogiliberti@gmail.com>
2018-05-22 07:43:26 -0300

    Se termina clase Dispositivo Inteligente y se agrega su respectivo test

--- src/main/java/martes/noche/DispositivoInteligente.java
+++ src/main/java/martes/noche/DispositivoInteligente.java
@@ -1,6 +1,8 @@
 package martes.noche;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -8,11 +10,13 @@ public class DispositivoInteligente extends Dispositivo {
     
 	List<Consumo> consumo;
     Actuador actuador;
+    List<Integer> periodos;
 
     public DispositivoInteligente(String nombre, int consumoHora, String estado) {
         super(nombre, consumoHora, estado);
         this.consumo = new ArrayList<Consumo>();
         this.actuador = null;
+        this.initializePeriods();
     }
 
     public void addConsumo(int watts, Date fechaInicio, Date fechaFinal) {
@@ -23,17 +27,6 @@ public class DispositivoInteligente extends Dispositivo {
         consumo.add(consumoI);
 
     }
-    public int getConsumoTotal(Date periodo) {
-        int total = 0;
-        for(int i=0; i< consumo.size(); i++) {
-        if(consumo.get(i).getFechaInicio().after(periodo)){
-        	total += consumo.get(i).getWatts();
-        	}
-        }
-        return total;
-    }
-
-
     
     public void encender() {
     	if(this.getEstado() != "encendido") {
@@ -60,4 +53,40 @@ public class DispositivoInteligente extends Dispositivo {
     public void setActuador(Actuador actuador) {
         this.actuador = actuador;
     }
+    
+    public int getConsumoTotal(Date fechaInicio, Date fechaFinal) {
+        int total = 0;
+        for(int i=0; i< consumo.size(); i++) {
+            if((consumo.get(i).getFechaInicio().after(fechaInicio) || (consumo.get(i).getFechaFinal()).before(fechaFinal))) {
+            	total += consumo.get(i).getWatts();
+            }
+        }
+        return total;
+    }
+    
+    public void initializePeriods( ) {
+    	periodos = new ArrayList<Integer>();
+    	for (int i = 0; i < 12; i++) {
+    		periodos.add(i, 0);
+    	}
+    }
+    
+    public void cleanPeriod() {
+    	Date date = new Date();
+    	Calendar c = Calendar.getInstance();
+    	c.setTime(date);
+    	c.add(Calendar.MONTH, -1);
+    	int watts = 0;
+    	for(int i = 0; i < consumo.size(); i++) {
+    		if(consumo.get(i).getFechaInicio().after(c.getTime())) {
+    			watts += consumo.get(i).getWatts();
+    		}
+    	}
+    	periodos.add(c.get(Calendar.MONTH), watts);
+    }
+    
+    public int getConsumoPeriodo(int mes) {
+    	return periodos.get(mes);
+    }
+    
 }
