98af48bcd5ef87536cf3ed7a1aa1e226e44c0c0c
Martin Gauna <martin.gauna@sovanta.com>
2018-09-11 19:21:12 -0300

    Se empieza con la persistencia de los sensores.

--- src/main/java/ar/edu/utn/frba/dds/Magnitud.java
+++ src/main/java/ar/edu/utn/frba/dds/Magnitud.java
@@ -1,13 +1,26 @@
 package ar.edu.utn.frba.dds;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table
 public class Magnitud {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotNull
     private long valor;
+    @NotNull
     private String magnitud;
 
     /**
      * Getters & Setters
      */
+
+    public int getId() { return id; }
+
     public long getValor() {
         return valor;
     }
