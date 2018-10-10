fbc92dc19a376ecff3d2693931fcf21c34b807f5
gdpepp <dgusti89@gmail.com>
2018-10-09 23:08:31 -0300

    caso de prueba 2 - Issue #31

--- src/test/java/db/DispositivoPersistenceTest.java
+++ src/test/java/db/DispositivoPersistenceTest.java
@@ -1,6 +1,7 @@
 package db;
 
 import ar.edu.utn.frba.dds.Cliente;
+import ar.edu.utn.frba.dds.Consumo;
 import ar.edu.utn.frba.dds.Transformador;
 import ar.edu.utn.frba.dds.Zona;
 import ar.edu.utn.frba.dds.dispositivo.DispositivoInteligente;
@@ -12,7 +13,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -27,6 +31,8 @@ public class DispositivoPersistenceTest {
     public void before() {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("TEST_PERSISTENCE_GONZALO");
         entityManager = factory.createEntityManager();
+
+
     }
 
     @Test
@@ -54,49 +60,70 @@ public class DispositivoPersistenceTest {
     }
 
     @Test
-    public void cambioGeolocalizacionTest() {
+    public void periodosEncendidoTest() throws ParseException {
 
         // creo un nuevo cliente
         Cliente cliente = new Cliente("CliName", "CliApell", "CliDom", "CliUser",
                 "CliPass", LocalDate.of(2018, 04, 28), "DNI",
                 87654321, 45555533, null, LocalDate.now(), true);
 
-        Zona almagro = new Zona("Almagro", -34.618680, -58.415423, 300);
-        Zona sanTelmo = new Zona("San Telmo", -34.6210356, -58.373654, 300);
-        Transformador tra1 = new Transformador(-34.618680, -58.415423, almagro);
-        Transformador tra2 = new Transformador(-34.618680, -58.415423, sanTelmo);
-        almagro.addTransformador(tra1);
-        sanTelmo.addTransformador(tra2);
-        cliente.setTransformador(tra1);
+        //creacion de dispositivo
+        DispositivoInteligenteHeladera dispInt = new DispositivoInteligenteHeladera("Heladera", 123, "activo", false);
+        cliente.addDispositivo(dispInt);
+
+        //consumos del dispositivo
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date d = sdf.parse("12/12/2018");
+        Date e = sdf.parse("18/12/2018");
+        Consumo cons1 = new Consumo(dispInt, 1200, d, e);
+        Date f = sdf.parse("19/12/2018");
+        Date g = sdf.parse("21/12/2018");
+        Consumo cons2 = new Consumo(dispInt, 2100, f, g);
+        dispInt.addConsumo(cons1);
+        dispInt.addConsumo(cons2);
 
         // persisto
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
-        entityManager.persist(almagro);
-        entityManager.persist(sanTelmo);
-        entityManager.persist(tra1);
-        entityManager.persist(tra2);
+        entityManager.persist(cons1);
+        entityManager.persist(cons2);
+        entityManager.persist(dispInt);
         entityManager.persist(cliente);
         transaction.commit();
 
-        //verifico que tiene asdignada la zona almagro
+        //mostrado por consola de consumos
 
         Cliente clientePersistido = null;
         clientePersistido = entityManager.find(Cliente.class, cliente.getId());
-        Zona almagroPersistida = null;
-        almagroPersistida = entityManager.find(Zona.class, almagro.getId());
-        Zona sanTelmoPersistida = null;
-        sanTelmoPersistida = entityManager.find(Zona.class, sanTelmo.getId());
+        DispositivoInteligente dispPersistido = null;
+        dispPersistido = entityManager.find(DispositivoInteligenteHeladera.class, dispInt.getId());
+        Consumo cons1Persistido = null;
+        cons1Persistido = entityManager.find(Consumo.class, cons1.getId());
+        Consumo cons2Persistido = null;
+        cons2Persistido = entityManager.find(Consumo.class, cons2.getId());
 
-        assertEquals(clientePersistido.getTransformador().getZona(), almagroPersistida);
-        assertNotEquals(clientePersistido.getTransformador().getZona(), sanTelmoPersistida);
+        assertEquals(clientePersistido.getNombre(), cliente.getNombre());
+        assertEquals(dispPersistido.getNombre(), dispInt.getNombre());
 
-        //hago el cambio de trasformador para que cambie de zona
-        cliente.cambiarTransformador(tra2);
+        System.out.println("----------------------------------");
+        System.out.println("Listado de intervalos de encendido");
+        System.out.println("----------------------------------");
 
-        //verifico el cambio de geolocalizacion
-        assertNotEquals(clientePersistido.getTransformador().getZona(), almagroPersistida);
-        assertEquals(clientePersistido.getTransformador().getZona(), sanTelmoPersistida);
+        for(Consumo c : dispPersistido.getConsumos()) {
+            System.out.println("Consumo: " + c.getWatts());
+            System.out.println("Inicio:  " + c.getFechaInicio());
+            System.out.println("Fin:     " + c.getFechaFinal() + "\n");
+        }
+
+        //hago el cambio de nombre de dispositivo
+
+        String nombrePrevio = dispInt.getNombre();
+        String nombreNuevo = "Condensador de flujo";
+        dispInt.setNombre(nombreNuevo);
+
+        //verifico el cambio de nombre
+        assertNotEquals(dispPersistido.getNombre(),nombrePrevio);
+        assertEquals(dispPersistido.getNombre(),nombreNuevo);
     }
 }
 
