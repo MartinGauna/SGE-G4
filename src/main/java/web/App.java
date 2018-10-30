a51ea15b2887a4c360d623037f6912bee00c72fb
Juan Martin Conde <juanma.conde@hotmail.com>
2018-10-30 16:23:47 -0300

    Primer acercamiento a pantalla para alta de dispositivos de administrador

--- src/main/java/web/App.java
+++ src/main/java/web/App.java
@@ -11,6 +11,7 @@ import ar.edu.utn.frba.dds.helpers.BackgroundProcesses;
 import ar.edu.utn.frba.dds.jsonParser.JsonParser;
 import spark.Spark;
 import spark.debug.DebugScreen;
+import web.controllers.Admin.AltaDispositivoController;
 import web.controllers.Admin.HogarController;
 import web.controllers.login.LoginController;
 
@@ -88,12 +89,16 @@ public class App
         staticFileLocation("/webResources");
         DebugScreen.enableDebugScreen();
 
-        HogarController.init();
-
-        LoginController.init();
+        startControllers();
 
     }
-    
+
+    public static void startControllers(){
+        HogarController.init();
+        LoginController.init();
+        AltaDispositivoController.init();
+    }
+
     public static App getInstance() {
     	if(instance == null) { 
     		instance = new App();
--- src/main/java/web/controllers/Admin/AltaDispositivoController.java
+++ src/main/java/web/controllers/Admin/AltaDispositivoController.java
@@ -1,6 +1,7 @@
 package web.controllers.Admin;
 
 
+import ar.edu.utn.frba.dds.dao.ClientDao;
 import ar.edu.utn.frba.dds.dao.DispositivoDao;
 import spark.ModelAndView;
 import spark.Request;
@@ -12,26 +13,31 @@ import web.controllers.mainController;
 import web.models.AltaDispositivoModel;
 
 public class AltaDispositivoController extends mainController {
-    private static final String ALTADISPOSITIVO = "/admin/altaDispositivo.html";
+
+    private static final String ALTADISPOSITIVO = "/admin/altaDispositivo.hbs";
     private static AltaDispositivoModel model;
 
-    private static DispositivoDao ddao = new DispositivoDao();
+    private static ClientDao clientDao = new ClientDao();
+    private static DispositivoDao dispositivoDao = new DispositivoDao();
 
     public static void init() {
         HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
-        Spark.get(Router.adminHogaresPath(), AltaDispositivoController::altaDispositivo,engine);
+        Spark.get(Router.altaDispositivoPath(), AltaDispositivoController::load,engine);
         initModel();
     }
 
-    private static ModelAndView altaDispositivo(Request request, Response response) {
+    private static ModelAndView load(Request request, Response response) {
         sessionExist(request, response);
-
-        return new ModelAndView (model, ALTADISPOSITIVO); // TODO add 'hogares' model
+        return new ModelAndView (model, ALTADISPOSITIVO); // TODO add 'Dispositivo' model
     }
 
     private static void initModel() {
 
+        model = new AltaDispositivoModel();
+    }
 
+    public static void crearDispositivo(){
+        System.out.println("Llegue aca");
     }
 
 }
