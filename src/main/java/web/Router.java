1ffd7bfe7cb2ea5da731e4efc380497cd63111de
Gonzalo Giliberti <gonzalogiliberti@gmail.com>
2018-10-30 18:49:10 -0300

    Se agrega modelo y contralos para los reportes

--- src/main/java/web/Router.java
+++ src/main/java/web/Router.java
@@ -10,6 +10,7 @@ public class Router {
     private static final String LOGOUT = "/logout";
     private static final String ADMIN_HOGARES = "/hogares";
     private static final String ALTA_DISPOSITIVO = "/altadispositivo";
+    private static final String REPORTES = "/reportes";
 
 
 
@@ -28,6 +29,8 @@ public class Router {
     public static String adminHogaresPath() { return ADMIN_HOGARES; }
 
     public static String altaDispositivoPath() { return ALTA_DISPOSITIVO; }
+
+    public static String reportesPath() { return REPORTES; }
 
 //    public static String hogares() { return ADMIN_HOGARES; }
 //    public static String hogares() { return ADMIN_HOGARES; }
---
+++ src/main/java/web/controllers/client/ReportesController.java
@@ -0,0 +1,117 @@
+package web.controllers.client;
+
+import ar.edu.utn.frba.dds.Cliente;
+import ar.edu.utn.frba.dds.Reporte;
+import ar.edu.utn.frba.dds.ReporteTransformador;
+import ar.edu.utn.frba.dds.Transformador;
+import ar.edu.utn.frba.dds.dao.ClientDao;
+import ar.edu.utn.frba.dds.dao.ReporteDao;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import web.Router;
+import web.models.ReportesModel;
+import web.controllers.mainController;
+import web.models.views.ReportesTable;
+import web.models.views.ReportesTrafosTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReportesController extends mainController {
+    private static final String REPORTES = "/client/reportes.hbs";
+    private static ReportesModel model;
+
+    private static ClientDao cdao = new ClientDao();
+    private static ReporteDao rdao = new ReporteDao();
+
+
+    public static void init() {
+        HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+        Spark.get(Router.reportesPath(), ReportesController::showReportes,engine);
+        initModel();
+    }
+
+    private static ModelAndView showReportes(Request request, Response response) {
+        sessionExist(request, response);
+
+        return new ModelAndView (model, REPORTES); // TODO add 'hogares' model
+    }
+
+    private static void initModel() {
+
+        model = new ReportesModel();
+
+        List<ReportesTable> tableReporte = new ArrayList<ReportesTable>();
+        List<ReportesTrafosTable> tableReporteTrafo = new ArrayList<ReportesTrafosTable>();
+
+        List<Reporte> rep = rdao.listReporte();
+        List<ReporteTransformador> repT = rdao.listReporteTrafo();
+
+        for ( Reporte r : rep) {
+            ReportesTable row = new ReportesTable();
+            row.setReporte(r);
+
+            tableReporte.add(row);
+        }
+        for ( ReporteTransformador r : repT) {
+            ReportesTrafosTable rowT = new ReportesTrafosTable();
+            rowT.setReporteTrafo(r);
+
+            tableReporteTrafo.add(rowT);
+        }
+
+        model.setTableReporte(tableReporte);
+        model.setTableReporteTrafo(tableReporteTrafo);
+    }
+
+    public void getReportesPorCliente(Cliente c) {
+
+        List<ReportesTable> tableReporte = new ArrayList<ReportesTable>();
+        List<Reporte> reportes = rdao.getReportePorCliente(c.getId());
+
+        model.cleanTableReporte();
+
+        for (Reporte r : reportes) {
+            ReportesTable row = new ReportesTable();
+            row.setReporte(r);
+            tableReporte.add(row);
+        }
+
+        model.setTableReporte(tableReporte);
+    }
+
+    public void getReportesPorTipo(int type) {
+
+        List<ReportesTable> tableReporte = new ArrayList<ReportesTable>();
+        List<Reporte> reportes = rdao.getReportePorCliente(type);
+
+        model.cleanTableReporte();
+
+        for (Reporte r : reportes) {
+            ReportesTable row = new ReportesTable();
+            row.setReporte(r);
+            tableReporte.add(row);
+        }
+
+        model.setTableReporte(tableReporte);
+    }
+
+    public void getReportesPorTrafo(Transformador trafo) {
+
+        List<ReportesTrafosTable> tableReporte = new ArrayList<ReportesTrafosTable>();
+        List<ReporteTransformador> reportes = rdao.getReportePorTransformador(trafo.getId());
+
+        model.cleanTableReporteTrafo();
+
+        for (ReporteTransformador r : reportes) {
+            ReportesTrafosTable row = new ReportesTrafosTable();
+            row.setReporteTrafo(r);
+            tableReporte.add(row);
+        }
+
+        model.setTableReporteTrafo(tableReporte);
+    }
+}
