b04814cf1a31ca4a609a5ff82e043edc92a98768
gdpepp <dgusti89@gmail.com>
2018-11-04 00:16:03 -0300

    POST de alta de dispositivo

--- src/main/java/web/controllers/Admin/AltaDispositivoController.java
+++ src/main/java/web/controllers/Admin/AltaDispositivoController.java
@@ -1,8 +1,10 @@
 package web.controllers.Admin;
 
 
+import ar.edu.utn.frba.dds.Cliente;
 import ar.edu.utn.frba.dds.dao.ClientDao;
 import ar.edu.utn.frba.dds.dao.DispositivoDao;
+import ar.edu.utn.frba.dds.dispositivo.Dispositivo;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -10,10 +12,15 @@ import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import web.Router;
 import web.controllers.MainController;
+import web.models.AlertModel;
 import web.models.AltaDispositivoModel;
+import web.models.views.HogaresTable;
+
+import java.util.List;
 
 public class AltaDispositivoController extends MainController {
     private static final String ALTADISPOSITIVO = "/admin/altaDispositivo.hbs";
+    private static AlertModel alert = new AlertModel(false,"",false);
 
     private static AltaDispositivoModel model;
 
@@ -23,6 +30,7 @@ public class AltaDispositivoController extends MainController {
     public static void init() {
         HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
         Spark.get(Router.altaDispositivoPath(), AltaDispositivoController::load,engine);
+        Spark.post(Router.altaDispositivoPath(),AltaDispositivoController::crearDispositivo,engine);
         initModel();
     }
 
@@ -34,10 +42,30 @@ public class AltaDispositivoController extends MainController {
     private static void initModel() {
 
         model = new AltaDispositivoModel();
+        List<Cliente> cls = clientDao.list();
+
+        for (Cliente c : cls) {
+            model.getClientes().add(c);
+        }
     }
 
-    public static void crearDispositivo(){
-        System.out.println("Llegue aca");
+    public static ModelAndView crearDispositivo(Request request, Response response){
+
+        String tipo = request.queryParams("tipo");
+        String disp = request.queryParams("dispositivo");
+        String nombre = request.queryParams("nombre");
+        String consumo = request.queryParams("consumoHora");
+        String estado = request.queryParams("estado");
+        String username = request.queryParams("cliente");
+
+        System.out.println(tipo);
+        System.out.println(disp);
+        System.out.println(nombre);
+        System.out.println(consumo);
+        System.out.println(estado);
+        System.out.println(username);
+
+        return new ModelAndView (model, ALTADISPOSITIVO);
     }
 
 }
--- src/main/java/web/models/AltaDispositivoModel.java
+++ src/main/java/web/models/AltaDispositivoModel.java
@@ -1,23 +1,33 @@
 package web.models;
 
+import ar.edu.utn.frba.dds.Cliente;
 import ar.edu.utn.frba.dds.dispositivo.*;
 import web.models.views.DispositivosTable;
 
-import java.util.List;
+import java.util.*;
 
 
 public class AltaDispositivoModel {
 
     private DispositivosTable dispositivosDisponibles;
+    private List<Cliente> clientes;
 
     public AltaDispositivoModel() {
         dispositivosDisponibles = new DispositivosTable();
+        clientes = new ArrayList<>();
     }
 
     public List<Dispositivo> getDispositivos() {
         return this.dispositivosDisponibles.getDispositivosDisponibles();
     }
 
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
 
 }
 
