b5002a865cc913f2b4f4bfa5e5bb11045372d66c
Matvivone <matiasvivone@hotmail.com>
2018-12-02 14:16:30 -0300

    test

--- src/main/java/web/App.java
+++ src/main/java/web/App.java
@@ -40,7 +40,7 @@ public class App
 
 //        LoadData.Load();
 
-        Spark.port(3306);
+        Spark.port(getHerokuAssignedPort());
         staticFileLocation("/webResources");
         DebugScreen.enableDebugScreen();
 
@@ -50,6 +50,14 @@ public class App
       //      GeneradorReportes.getInstance().generarReportes();
      //   }
 
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 
     private static void startControllers(){
